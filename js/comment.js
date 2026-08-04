b5a9b6c80e1d14fa8764ed6bd48a78ed6704ed67
ieasxu1407 <ieasxu1407@naver.com>
2026-08-04 14:33:46 +0900

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -1,17 +1,16 @@
-(function() {
+window.addEventListener('DOMContentLoaded', function() {
     // 1. 댓글이 들어갈 div 그릇을 동적으로 생성
     var container = document.createElement('div');
     container.id = 'lv-container';
     container.setAttribute('data-id', 'city');
-    container.setAttribute('data-uid', 'gduqz1LsuD051rtms296'); // 여기에 본인 UID 삽입
+    container.setAttribute('data-uid', 'gduqz1LsuD051rtms296'); // 꼭 본인 UID로 변경
 
-    // 2. 현재 이 스크립트 파일이 호출된 위치 바로 앞에 div 그릇을 끼워 넣음
-    var currentScript = document.currentScript;
-    currentScript.parentNode.insertBefore(container, currentScript);
+    // 2. HTML 본문의 맨 마지막(또는 원하는 영역)에 div 강제 삽입
+    document.body.appendChild(container);
 
-    // 3. 라이브리 서버에서 댓글 위젯 시스템 불러오기
+    // 3. 라이브리 서버에서 댓글 위젯 시스템 호출
     var j = document.createElement('script');
     j.src = 'https://livere.com';
     j.async = true;
-    currentScript.parentNode.insertBefore(j, currentScript);
-})();
+    document.body.appendChild(j);
+});
